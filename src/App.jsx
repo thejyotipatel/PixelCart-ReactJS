01b95254c6452b2b06158deaba6055928f98f9f1
thejyotipatel <jyotipatelfyit77100@gmail.com>
2024-02-08 10:13:09 -0800

    pages stru

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from './pages'
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>
+        Hello world! Vite + React
+      </h1>
+    </>
+  )
+}
+
+export default App
